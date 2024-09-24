96ccbefb6a70967b98ee4da15de5ffb498280583
naybyal <officiallynabiel@gmail.com>
2024-09-25 01:07:18 +0530

    refactor: Lexical Analyzer

--- LexicalAnalysisUsingC/test/lex.c
+++ LexicalAnalysisUsingC/test/lex.c
@@ -2,10 +2,12 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
+
 void main() {
     FILE *input, *output;
     int line = 1, token_no = 0;
-    char ch, str[20];
+    char ch, prev_ch = ' '; 
+    char str[20];
     char keyword[6][10] = {"int", "main", "if", "else", "do", "while"};
 
     input = fopen("input.txt", "r");
@@ -17,16 +19,30 @@ void main() {
     }
 
     fprintf(output, "Line no.\tToken no.\tToken\t\tLexeme\n\n");
+    
     while ((ch = fgetc(input)) != EOF) {
-        if (strchr("+-*/", ch))
-            fprintf(output, "%7d\t%7d\tOperator\t%7c\n", line, token_no++, ch);
-        else if (strchr(";{}()?!@%", ch))
+        if (strchr("+-*/", ch)) {
+            if (ch == prev_ch) {
+                fprintf(output, "%7d\t%7d\tUnary Operator\t%7c\n", line, token_no++, ch);
+                // prev_ch = ch;
+            } else {
+                fprintf(output, "%7d\t%7d\tOperator\t%7c\n", line, token_no++, ch);
+            }
+        } else if (strchr(";{}()?!@%", ch)) {
             fprintf(output, "%7d\t%7d\tSpecial symbol\t%7c\n", line, token_no++, ch);
-        else if (isdigit(ch))
-            fprintf(output, "%7d\t%7d\tDigit\t\t%7c\n", line, token_no++, ch);
-        else if (ch == '\n')
+        } else if (isdigit(ch)) {
+            ungetc(ch, input);
+            int i = 0;
+            while (isdigit(ch = fgetc(input))) {
+                str[i++] = ch;
+            }
+            str[i] = '\0';
+            ungetc(ch, input); 
+            
+            fprintf(output, "%7d\t%7d\tDigit\t\t%7s\n", line, token_no++, str);
+        } else if (ch == '\n') {
             line++;
-        else if (isalpha(ch)) {
+        } else if (isalpha(ch)) {
             int i = 0;
             do {
                 str[i++] = ch;
@@ -43,8 +59,10 @@ void main() {
             }
             fprintf(output, "%7d\t%7d\t%s\t%7s\n", line, token_no++, is_keyword ? "Keyword" : "Identifier", str);
         }
+
+        prev_ch = ch;
     }
+
     fclose(input);
     fclose(output);
 }
-
